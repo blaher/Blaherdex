3cf2321f3e16ec1ab4c060b28806ab47b2f903c8
Benjamin Young <ben@tkg.com>
2012-07-23 21:49:59 -0400

    Calling it a night

--- inc/lib/blaherdex.php
+++ inc/lib/blaherdex.php
@@ -1,13 +1,22 @@
 <?php
 	class Blaherdex
 	{
-		private $objDatabase;
+		private $objDatabase, $objYQL;
 		
 		public function __construct(PDO $objDatabase)
 		{
 			$this->objDatabase=$objDatabase;
+			$this->objYQL=new YQL();
 			
 			echo 'test';
+			
+			$var=$this->objYQL->query
+			(
+				'SELECT *
+					FROM yahoo.finance.quotes
+					WHERE symbol IN ("YHOO","AAPL","GOOG","MSFT")
+				;'
+			);var_dump($var);
 		}
 	}
 ?>
--- inc/lib/yql.php
+++ inc/lib/yql.php
@@ -3,7 +3,19 @@
 	{
 		public function __construct()
 		{
+			// Do I need to do anything here?
+		}
+		
+		public function query($strQuery)
+		{
+			$strURL
+				='http://query.yahooapis.com/v1/public/yql?q='
+					.urlencode($strQuery)
+					.'&diagnostics=true'
+					.'&env=http%3A%2F%2Fdatatables.org%2Falltables.env'
+			;
 			
+			return $strURL;
 		}
 	}
 ?>
